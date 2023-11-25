2819f4f2c134158b23b71161d71435746ca90c4c
JFmaia <jfmaia.dev@gmail.com>
2023-11-25 02:16:34 -0300

    style: Using quasar for layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
-    {{ count }}
-    <button @click="increment()">
-      +
-    </button>
+  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 20px;">
+    <q-card
+      dark
+      bordered
+      class="bg-grey-6 my-card"
+    >
+      <q-card-section class="section-card">
+        {{ count }}
+      </q-card-section>
+    </q-card>
+    <q-btn
+      @click="increment()"
+      color="#ffffff"
+      text-color="black"
+      label="Increment"
+    />
   </div>
 </template>
 
@@ -18,4 +29,7 @@ function increment(){
 </script>
 
 <style scoped>
+.section-card {
+  padding: 10px 20px;
+}
 </style>
